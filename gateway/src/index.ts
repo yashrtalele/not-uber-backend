c1d5fcfd03f441a4abb2e68286132ddf4afb943a
Yash Talele <yashrtalele@gmail.com>
2024-12-10 17:54:49 +0530

    Changed singleQuote to false in .prettierrc

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -1,8 +1,8 @@
-import express, { Express, Request, Response } from 'express';
-import expressProxy from 'express-http-proxy';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+import express, { Express, Request, Response } from "express";
+import expressProxy from "express-http-proxy";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 dotenv.config();
 const app: Express = express();
@@ -12,12 +12,12 @@ const PORT = process.env.PORT || 9756;
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/user', expressProxy('http://localhost:3001'));
-app.use('/driver', expressProxy('http://localhost:3002'));
-app.use('/ride', expressProxy('http://localhost:3003'));
+app.use("/user", expressProxy("http://localhost:3001"));
+app.use("/driver", expressProxy("http://localhost:3002"));
+app.use("/ride", expressProxy("http://localhost:3003"));
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Gateway is up and running!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Gateway is up and running!");
 });
 
 app.listen(PORT, () => {
--- user/src/index.ts
+++ user/src/index.ts
@@ -1,7 +1,7 @@
-import express, { Express, Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app: Express = express();
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 9757;
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('User service is up and running!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("User service is up and running!");
 });
 
 app.listen(PORT, () => {
