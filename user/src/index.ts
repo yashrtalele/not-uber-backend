6669fd8b8160105f431b4adf523d538fd4b68bb2
Yash Talele <yashrtalele@gmail.com>
2024-12-10 16:21:24 +0530

    Setup user server and added skeleton to the user server side

---
+++ user/src/index.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const app: Express = express();
+const PORT = process.env.PORT || 9757;
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('User service is up and running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`User service listening on port ${PORT}`);
+});
