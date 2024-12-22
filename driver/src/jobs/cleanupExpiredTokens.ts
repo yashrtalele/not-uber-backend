1e16f17e89332a7209ec7128fc6626156508f3d0
Yash Talele <yashrtalele@gmail.com>
2024-12-22 16:10:55 +0530

    Added cron job to delete blacklisted tokens

---
+++ driver/src/jobs/cleanupExpiredTokens.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+import cron from "node-cron";
+
+const prisma = new PrismaClient();
+
+cron.schedule("* * * * *", async () => {
+  const now = new Date();
+  const expirationTime = new Date(now.getTime() - 3600 * 1000);
+  await prisma.blacklistTokens.deleteMany({
+    where: {
+      createdAt: {
+        lt: expirationTime,
+      },
+    },
+  });
+});
