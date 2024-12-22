650edc3d7c82b010cc8d2699f13162f1a99b603a
Yash Talele <yashrtalele@gmail.com>
2024-12-22 16:11:24 +0530

    Added routes and cron job to the index file

--- driver/src/index.ts
+++ driver/src/index.ts
@@ -1,17 +1,22 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import { router as driverRouter } from "./routes/driver";
+import "./jobs/cleanupExpiredTokens";
+import { router as twilioRouter } from "./routes/twilio";
 
 dotenv.config();
 const app: Express = express();
 const PORT = process.env.PORT || 9758;
+app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Driver service is up and running!");
-});
+app.use("/", driverRouter);
+app.use("/verify", twilioRouter);
 
 app.listen(PORT, () => {
   console.log(`Driver service listening on port ${PORT}`);
