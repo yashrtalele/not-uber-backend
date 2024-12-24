6d45f83effd77b71eeb411f0922aae6cf74c142f
Yash Talele <yashrtalele@gmail.com>
2024-12-24 23:47:08 +0530

    Added routes for map and rides
    Signed-off-by: Yash Talele <yashrtalele@gmail.com>

--- ride/src/index.ts
+++ ride/src/index.ts
@@ -1,17 +1,21 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import cors from "cors";
+import { router as rideRouter } from "./routes/rides";
+import { router as mapRouter } from "./routes/map";
 
 dotenv.config();
 const app: Express = express();
 const PORT = process.env.PORT || 9759;
+app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Ride service is up and running!");
-});
+app.use("/", rideRouter);
+app.use("/map", mapRouter);
 
 app.listen(PORT, () => {
   console.log(`Ride service listening on port ${PORT}`);
