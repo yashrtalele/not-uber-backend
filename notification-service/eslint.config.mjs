a27fbfe7f18bd78f07296e063a9810571f0542ec
Yash Talele <yashrtalele@gmail.com>
2024-12-26 18:37:24 +0530

    Added ts rule to skip explicit any
    Signed-off-by: Yash Talele <yashrtalele@gmail.com>

--- notification-service/eslint.config.mjs
+++ notification-service/eslint.config.mjs
@@ -2,11 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": ["off"],
+    },
+  },
 ];
